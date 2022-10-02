74b37b5c9b7f2385647a8596c65ffa610606c455
ik now <iknowmail779@gmail.com>
2022-10-01 20:04:40 -0400

    update

--- src/App.js
+++ src/App.js
@@ -26,23 +26,31 @@ function handleConvertTo(){
   setValueTo(inputTo.current.value)
   setValueFrom(inputTo.current.value/rate)
 }
+function handleSetValue(){
+   setTheFrom(this.props.value)
+}
   return (
     <>
     <section>
-      <div className="miniSection">
+      <div className="miniSection mini1">
         <span>
           {theFrom}
         </span>
         <input onChange={handleConvertFrom} ref={inputFrom} value={valueFrom} type='number' />
       </div>
-      <button onClick={handleSwitch}>switch</button>
-      <div className="miniSection">
+      <button onClick={handleSwitch}>🔁</button>
+      <div className="miniSection mini2">
         <span>
           {theTo}
         </span>
         <input onChange={handleConvertTo} ref={inputTo} value={valueTo} type='number' />
       </div>
     </section>
+    <ul>
+      <li onClick={handleSetValue} prop value='lol'>  my value is lol</li>
+      <li onClick={handleSetValue} value='bro'>  my value is bro</li>
+      <li onClick={handleSetValue} value='lmao'>  my value is lmao</li>
+    </ul>
     </>
   );
 }
