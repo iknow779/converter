0ac0924b0733a8a385d1df5b81792424c90b88c6
ik now <iknowmail779@gmail.com>
2022-10-14 12:52:39 -0400

    Progress

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css'
 
 function App() {
 const[theFrom,setTheFrom] = useState('Meter')
-const[rate,setRate] = useState(100)
+const[rate,setRate] = useState()
 const[theTo,setTheTo] = useState('Centimeter')
 const[valueFrom,setValueFrom] = useState('')
 const[valueTo,setValueTo] = useState('')
@@ -29,9 +29,71 @@ function handleConvertTo(){
 useEffect(() => {
     if(theFrom==='Meter' && theTo==='Kilometer'){
       setRate(0.001)
+      setValueFrom('')
+      setValueTo('')
     }
     else if(theFrom==='Meter' && theTo==='Miles'){
       setRate(1/1069.34)
+      setValueFrom('')
+      setValueTo('')
+    }
+    else if(theFrom==='Meter' && theTo==='Centimeter'){
+      setRate(100)
+      setValueFrom('')
+      setValueTo('')
+    }
+    else if(theFrom==='Kilometer' && theTo==='Centimeter'){
+      setRate(100000)
+      setValueFrom('')
+      setValueTo('')
+    }
+    else if(theFrom==='Kilometer' && theTo==='Miles'){
+      setRate(1.60934)
+      setValueFrom('')
+      setValueTo('')
+    }
+    else if(theFrom==='Kilometer' && theTo==='Meter'){
+      setRate(1000)
+      setValueFrom('')
+      setValueTo('')
+    }
+    else if(theFrom==='Miles' && theTo==='Meter'){
+      setRate(1609.34)
+      setValueFrom('')
+      setValueTo('')
+    }
+    else if(theFrom==='Miles' && theTo==='Kilometer'){
+      setRate(1/1.60934)
+      setValueFrom('')
+      setValueTo('')
+    }
+    else if(theFrom==='Miles' && theTo==='Centimeter'){
+      setRate(160934)
+      setValueFrom('')
+      setValueTo('')
+    }
+    else if(theFrom==='Centimeter' && theTo==='Miles'){
+      setRate(1/160934)
+      setValueFrom('')
+      setValueTo('')
+    }    
+    else if(theFrom==='Centimeter' && theTo==='Meter'){
+      setRate(0.01)
+      setValueFrom('')
+      setValueTo('')
+    }
+    else if(theFrom==='Centimeter' && theTo==='Kilometer'){
+      setRate(0.00001)
+      setValueFrom('')
+      setValueTo('')
+    }
+    
+    
+    
+    else if(theFrom===theTo){
+      setRate(1)
+      setValueFrom('')
+      setValueTo('')
     }
 },[theFrom,theTo])
   return (
@@ -40,9 +102,10 @@ useEffect(() => {
       <div className="miniSection mini1">
         <span>
         <select value={theFrom} onChange={e=>(setTheFrom(e.target.value))}>
-            <option>{theFrom}</option>
             <option>Miles</option>
             <option>Kilometer</option>
+            <option>Centimeter</option>
+            <option>Meter</option>
           </select>
         </span>
         <input onChange={handleConvertFrom} ref={inputFrom} value={valueFrom} type='number' />
@@ -51,9 +114,10 @@ useEffect(() => {
       <div className="miniSection mini2">
         <span>
           <select value={theTo} onChange={e=>(setTheTo(e.target.value))}>
-            <option>{theTo}</option>
             <option>Miles</option>
             <option>Kilometer</option>
+            <option>Meter</option>
+            <option>Centimeter</option>
           </select>
         </span>
         <input onChange={handleConvertTo} ref={inputTo} value={valueTo} type='number' />
